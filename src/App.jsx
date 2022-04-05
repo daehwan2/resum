13ee1bed83516c95006afedf05440a161fa40cea
daehwan2 <dhdh0482@naver.com>
2022-04-06 01:21:54 +0900

    Feat: introduce 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import Education from "./components/Education";
+import Introduce from "./components/Introduce";
 import Profile from "./components/Profile";
+import Skill from "./components/Skill";
 
 function App() {
   return (
-    <div className="text-[16px] md:text-[20px]">
+    <div className="text-[16px] md:text-[20px] md:w-[750px] md:mx-[auto]">
       <Profile />
+      <hr className="my-[10px]" />
+      <Introduce />
+      <Skill />
+      <Education />
     </div>
   );
 }
